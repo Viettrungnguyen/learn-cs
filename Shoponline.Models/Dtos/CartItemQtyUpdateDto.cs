010939ca4d892802e53358aa8119be4120ec06a3
Trung Nguyen <trung.nguyen@kiaisoft.com>
2023-11-28 17:04:18 +0700

    dto and repo list product

---
+++ Shoponline.Models/Dtos/CartItemQtyUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoponline.Models.Dtos
+{
+	public class CartItemQtyUpdateDto
+	{
+		public int CartitemId { get; set; }
+		public int Qty { get; set;}
+	}
+}
