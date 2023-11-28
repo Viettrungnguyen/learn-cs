f83184e598a486e46ec1f8df0772f23791e66e3f
Trung Nguyen <trung.nguyen@kiaisoft.com>
2023-11-28 17:59:20 +0700

    blazor products

--- ShopOnline.Web/Pages/ProductsBase.cs
+++ ShopOnline.Web/Pages/ProductsBase.cs
@@ -1,6 +1,19 @@
-﻿namespace ShopOnline.Web.Pages
+﻿using Microsoft.AspNetCore.Components;
+using Shoponline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+
+namespace ShopOnline.Web.Pages
 {
-	public class ProductsBase
+	public class ProductsBase:ComponentBase
 	{
+		[Inject]
+		public IProductService ProductService { get; set; }
+
+		public IEnumerable<ProductDto> Products { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			Products = await ProductService.GetItems();
+		}
 	}
 }
